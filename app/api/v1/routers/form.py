91b9832e4535d83adb9336764338e6845d409bad
Jasmine <jasminechiang6312@gmail.com>
2026-09-08 00:43:10 +0800

    add user register and login

--- app/api/v1/routers/form.py
+++ app/api/v1/routers/form.py
@@ -6,6 +6,5 @@ from datetime import datetime, timedelta
 from app.core.jwt import create_access_token, decode_access_token
 from app.core.deps import verify_token, return_payload
 import re
-import pytz
 
 router = APIRouter()
