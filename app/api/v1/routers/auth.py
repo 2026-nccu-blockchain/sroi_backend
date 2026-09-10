32224732f14bfea58afa7a1851e6ff98054563a2
supbbro <joyeeeeee423@gmail.com>
2026-09-10 12:22:06 +0800

    feat: add form builder persistence API

--- app/api/v1/routers/auth.py
+++ app/api/v1/routers/auth.py
@@ -31,12 +31,15 @@ def is_strong_password(password: str) -> bool:
 def user_login(data: LoginRequest, db: Session = Depends(get_db)) -> dict:
     if not Account.verify_email(data.email):
         raise APIException(400, "10007", "incorrect email format")
-    user = db.query(Account).filter(Account.email == data.email and Account.is_delete == False).first()
+    user = db.query(Account).filter(
+        Account.email == data.email,
+        Account.is_delete.is_(False),
+    ).first()
     if user is None:
         raise APIException(400, "10001", "user not found")
     if not user.verify_password(data.password):
         raise APIException(400, "10002", "invalid password")
-    payload = {"id": f"{user.uuid}", "role": f"{user.role.value}"}
+    payload = {"id": user.user_id, "role": user.role.value}
     token = create_access_token(payload)
     
     return APIResponse(
@@ -53,13 +56,16 @@ def user_register(data: RegisterRequest, db: Session = Depends(get_db)) -> dict:
         raise APIException(400, "10007", "incorrect email format")
     if not is_strong_password(data.password):
         raise APIException(400, "10010", "password is not strong")
-    if db.query(Account).filter(Account.email == data.email and Account.is_delete == False).first() is not None:
+    if db.query(Account).filter(
+        Account.email == data.email,
+        Account.is_delete.is_(False),
+    ).first() is not None:
         raise APIException(400, "10006", "register duplicate")
     new_account = Account(
         email=data.email,
         hash_password=" ", #不能是null，下面才會設密碼,
         name=data.name,
-        role=Role.NON_AUTH
+        role=Role.UNVERIFIED
     )
     new_account.set_password(data.password)
     db.add(new_account)
