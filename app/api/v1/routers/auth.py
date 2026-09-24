3e2e411c0d92b81093d003e9609859c2e8bea700
Jasmine <jasminechiang6312@gmail.com>
2026-09-24 16:24:26 +0800

    add: change email and password

--- app/api/v1/routers/auth.py
+++ app/api/v1/routers/auth.py
@@ -10,6 +10,8 @@ from app.core.deps import verify_token, return_payload
 from app.schemas.auth import (
     LoginRequest,
     RegisterRequest,
+    ChangeEmailRequest,
+    ChangePasswordRequest
 )
 import re
 
@@ -52,7 +54,7 @@ def user_register(data: RegisterRequest, db: Session = Depends(get_db)) -> dict:
     if not Account.verify_email(data.email):
         raise APIException(400, "10007", "incorrect email format")
     if db.query(Account).filter(Account.email == data.email, Account.is_delete == False).first() is not None:
-            raise APIException(400, "10006", "register duplicate")
+        raise APIException(400, "10006", "register duplicate")
     if not is_strong_password(data.password):
         raise APIException(400, "10010", "password is not strong")
     new_account = Account(
@@ -65,6 +67,50 @@ def user_register(data: RegisterRequest, db: Session = Depends(get_db)) -> dict:
     db.add(new_account)
     db.commit()
     db.refresh(new_account)
+
+    return APIResponse(
+        status_code="00000",
+        message="success",
+        response_datetime=datetime.now(),
+    )
+
+
+@router.put("/user/change_email", response_model=APIResponse, response_model_exclude_none=True)
+def change_email(request: Request, data: ChangeEmailRequest, db: Session = Depends(get_db)) -> dict:
+    verify_token(request)
+    payload = return_payload(request)
+    user_id = payload["user_id"]
+    user = db.query(Account).filter(Account.user_id == user_id, Account.is_delete == False).first()
+    if user is None:
+        raise APIException(404, "10001", "user not found")
+    if not Account.verify_email(data.email):
+        raise APIException(400, "10007", "incorrect email format")
+    if db.query(Account).filter(Account.email == data.email, Account.is_delete == False).first() is not None:
+        raise APIException(400, "10006", "register duplicate")
+    user.email = data.email
+    db.commit()
+
+    return APIResponse(
+        status_code="00000",
+        message="success",
+        response_datetime=datetime.now(),
+    )
+
+
+@router.put("/user/change_password", response_model=APIResponse, response_model_exclude_none=True)
+def change_password(request: Request, data: ChangePasswordRequest, db: Session = Depends(get_db)) -> dict:
+    verify_token(request)
+    payload = return_payload(request)
+    user_id = payload["user_id"]
+    user = db.query(Account).filter(Account.user_id == user_id, Account.is_delete == False).first()
+    if user is None:
+        raise APIException(404, "10001", "user not found")
+    if not user.verify_password(data.old_password):
+        raise APIException(400, "10002", "invalid password")
+    if not is_strong_password(data.new_password):
+        raise APIException(400, "10010", "password is not strong")
+    user.set_password(data.new_password)
+    db.commit()
 
     return APIResponse(
         status_code="00000",
